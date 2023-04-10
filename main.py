e2af2708a94bdc75b57ecc498d6bc1798cea0300
Wilson Canuto Programador Front-End <99556299+wilsoncanuto@users.noreply.github.com>
2023-04-10 09:27:08 -0300

    calculadora simples
    calculadora simples

---
+++ main.py
@@ -0,0 +1,39 @@
+# Receber um número
+# Receber um operador
+# Receber um outro número
+# Mostar o resultado
+# Calculadora simples7
+
+import operator
+
+dict_operadores = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv
+
+}
+print("Calculadora simples")
+print("\nDigite um numero:")
+numero1 = int(input())
+
+
+print("Digite o operador:")
+operador = input()
+
+
+print("Digite segundo numero:")
+numero2 = int (input())
+
+
+print(dict_operadores[operador](numero1,numero2))
+
+
+
+
+
+
+
+
+
+
